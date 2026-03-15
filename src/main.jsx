69a00fa7b6f65847505d04e742f462979fb8cbf0
Jan Ricica <janricica@ufm.edu>
2026-03-15 17:35:32 -0400

    fix main

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,13 @@
-import { Provider } from 'react-redux';
-import store from './store';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import { Provider } from 'react-redux'
+import store from './store.js'
 
-<Provider store={store}>
-  <App />
-</Provider>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+)
